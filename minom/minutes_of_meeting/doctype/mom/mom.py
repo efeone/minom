904245a748b2cb405f3142e0a0102f54c5efa221
shamilnk91 <shamilnk91@gmail.com>
2022-08-16 12:17:03 +0530

    feat:Showing Pending Actions

--- minom/minutes_of_meeting/doctype/mom/mom.py
+++ minom/minutes_of_meeting/doctype/mom/mom.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2022, efeone Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class MOM(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_pending_actions(project):
+	'''getting uncompleted tasks
+	   output : list of uncompleted tasks according to the project
+	'''
+	return frappe.get_all('Task', filters = {'project': project, 'status': ['!=' , 'Completed']}, fields = ['subject', 'priority', 'description'])
+	
+	
