06a8977719c41890e5f1b89bf945f031ff4b01d0
MhmdSinanKT <muhammed786sinu000@gmail.com>
2022-08-16 13:03:51 +0530

    feat:Show last MOM Actions and Attendees on demand in the MOM, added a naming series and a function to calculate meeting duration

--- minom/minutes_of_meeting/doctype/mom/mom.js
+++ minom/minutes_of_meeting/doctype/mom/mom.js
@@ -2,18 +2,41 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('MOM', {
-	// refresh: function(frm) {
 
-	// }
+	from_time:function(frm) {
+		if(frm.doc.to_time)
+			calculate_time(frm);
+	},
+
+	to_time: function(frm) {
+		if(frm.doc.from_time) 	
+			calculate_time(frm);
+	},
+
+	review_last_mom: function (frm) {
+		if (frm.doc.review_last_mom == 1) {
+			frm.call('get_last_mom')
+		}
+		else
+			frm.clear_table('last_attendees')
+			frm.clear_table('last_actions')
+	}
 });
 
 frappe.ui.form.on('Attendees', {
-	user(frm,cdt,cdn) {
+	user(frm, cdt, cdn) {
 		set_filters(frm);
 	}
 })
 
-let set_filters = function (frm) {
+let calculate_time = function(frm) {
+	let to_time = new Date(frm.doc.to_time)
+	let from_time = new Date(frm.doc.from_time)
+	let duration = ((to_time.getTime() - from_time.getTime())/1000)/60
+	frm.set_value('meeting_duration', duration)
+}
+
+let set_filters = function(frm) {
 	if (frm.doc.attendees) {
 		var attendees = '';
 		frm.doc.attendees.forEach(function (attendee, i) {
--- minom/minutes_of_meeting/doctype/mom/mom.py
+++ minom/minutes_of_meeting/doctype/mom/mom.py
@@ -1,8 +1,34 @@
 # Copyright (c) 2022, efeone Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class MOM(Document):
-	pass
+
+	@frappe.whitelist()
+	def get_last_mom(self):
+		if self.project:
+			check = frappe.db.exists('MOM', { 'project': self.project})
+			if check:
+				last_mom = frappe.get_last_doc('MOM')
+				for i in last_mom.attendees:
+					if(i.attended == 1):
+						self.append('last_attendees', {
+							'user': i.user,
+							'full_name': i.full_name,
+							'attended': i.attended
+						})
+				
+				for i in last_mom.actions:
+					self.append('last_actions', {
+						'subject': i.subject,
+						'priority': i.priority,
+						'description': i.description
+					})			
+			else:
+				frappe.msgprint('There are no MOMs for the Selected Project')
+				self.review_last_mom = 0
+		else:
+			frappe.msgprint('Select a project to fetch it\'s last MOM')
+			self.review_last_mom = 0
